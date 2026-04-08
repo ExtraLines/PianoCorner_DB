6accb24bf57f82622694b54ea818f0cdc31b029b
ExtraLines <kevinc2k16@gmail.com>
2026-04-07 23:53:20 -0400

    testsql

--- queries/test.sql
+++ queries/test.sql
@@ -1,1 +1,5 @@
-SELECT * FROM song_to_artist LIMIT 30;
+SELECT * FROM songs LIMIT 10;
+SELECT * FROM artists LIMIT 10;
+SELECT * FROM sources LIMIT 10;
+SELECT * FROM song_to_artist LIMIT 10;
+SELECT * FROM song_to_source LIMIT 10;
